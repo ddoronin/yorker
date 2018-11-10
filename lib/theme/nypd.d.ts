8b628dbef0e658ffd8c4379c0dcf992e29f767b5
Dima Doronin <doronindm@gmail.com>
2018-11-10 00:47:34 -0500

    ignore lib folder

--- lib/theme/nypd.d.ts
+++
@@ -1,9 +0,0 @@
-import { ITheme } from '../yorker';
-export declare class NYPDTheme implements ITheme {
-    see(something: string, dateTime: Date): void;
-    say(something: string, dateTime: Date, ms: number): void;
-    yell(error: Error, something: string, dateTime: Date, ms: number): void;
-    private print;
-    private width;
-    private format;
-}
